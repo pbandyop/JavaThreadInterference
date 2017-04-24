a4479e5bd3cd790831c77b2a67fffa0e725ac401
Payel Bandyopadhyay <payel@Lings-MacBook-Pro.local>
2017-04-24 17:10:43 -0500

    Adding data file

---
+++ Data.java
@@ -0,0 +1,18 @@
+public class Data {
+
+	  private int value;
+
+	  public int getValue() {
+	    return value;
+	  }
+
+/*	  public void setValue(int value) {
+	   this.value = value;
+	  } */
+	  
+	  public int setValue(int value) {
+		   this.value = value;
+		   return value;
+		  }
+
+}
